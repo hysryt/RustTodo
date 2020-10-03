3681553c7a0101d7c3a6c5b3985336f80356b032
hysryt <hysryt@gmail.com>
2020-10-03 20:03:41 +0900

    add: todoリストをDBから取得

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
 use askama::Template;
-use thiserror::Error;
 use r2d2::Pool;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::params;
+use thiserror::Error;
 
 #[derive(Debug)]
 struct TodoEntry {
@@ -21,21 +21,31 @@ struct IndexTemplate {
 enum MyError{
     #[error("Failed to render HTML")]
     AskamaError(#[from] askama::Error),
+
+    #[error("Failed to get connection")]
+    ConnectionPoolError(#[from] r2d2::Error),
+
+    #[error("Failed SQL execution")]
+    SQLiteError(#[from] rusqlite::Error),
 }
 
 impl ResponseError for MyError {}
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, MyError> {
+async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    let mut statement = conn.prepare("SELECT id, text FROM todo")?;
+    let rows = statement.query_map(params![], |row| {
+        let id = row.get(0)?;
+        let text = row.get(1)?;
+        Ok(TodoEntry { id, text })
+    })?;
+
     let mut entries = Vec::new();
-    entries.push(TodoEntry{
-        id: 1,
-        text: "First entry".to_string(),
-    });
-    entries.push(TodoEntry{
-        id: 2,
-        text: "Second entry".to_string(),
-    });
+    for row in rows {
+        entries.push(row?);
+    }
+
     let html = IndexTemplate { entries };
     let response_body = html.render()?;
     Ok(HttpResponse::Ok()
